de5d6674c1f35f9cb21b229bcbc854d875f85c94
Swapnanil <135691331+swapnanil1@users.noreply.github.com>
2023-09-13 11:36:18 +0530

    Update init.lua to support wsl-clipboard

--- init.lua
+++ init.lua
@@ -5,3 +5,20 @@
 --   pattern = "*",
 --   command = "tabdo wincmd =",
 -- })
+
+-- Define a Lua table for clipboard settings
+local clipboard = {
+   name = 'WslClipboard',
+   copy = {
+      ['+'] = 'clip.exe',
+      ['*'] = 'clip.exe',
+   },
+   paste = {
+      ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+      ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+   },
+   cache_enabled = 0,
+}
+
+-- Assign the table to a Neovim global variable
+vim.g.clipboard = clipboard
